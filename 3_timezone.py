45be6bd47a70ed999495b46e45e818d1fda1d230
raph <me@raph.ws>
2019-02-06 22:05:06 +1100

    More stuff

--- 3_timezone.py
+++ 3_timezone.py
@@ -3,6 +3,8 @@ from dateutil import tz
 
 samoa_tz = tz.gettz('Pacific/Apia')
 a_date = datetime(2011, 12, 29, tzinfo=samoa_tz)
-print(a_date.isoformat())
-a_date = a_date + timedelta(days=1)
-print(a_date.isoformat())
+
+a_date_as_utc = a_date.astimezone(tz.UTC)
+a_date_as_utc += timedelta(days=1)
+
+print(a_date_as_utc.astimezone(samoa_tz))
